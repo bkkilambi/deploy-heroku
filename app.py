336957dee0ae2125085298d899d1ea443e0c5e32
Bhavana Kilambi <bhavana.kilambi@arm.com>
2021-11-14 10:48:41 +0000

    Added all required files

---
+++ app.py
@@ -0,0 +1,52 @@
+# import Flask class from the flask module
+from flask import Flask, request
+import joblib
+import numpy as np
+from flask import app, render_template
+import pandas as pd
+from functools import reduce
+from flask import jsonify
+
+
+# Create Flask object to run
+app = Flask(__name__)
+
+# Load the model from the file
+xgboost_model = joblib.load('model/xgboost_model.pkl')
+item_final_rating = joblib.load('model/item_final_rating')
+prod_tfidf = joblib.load('model/prod_tfidf')
+
+@app.route('/')
+def home():
+    return "Sentiment based Recommendation System Deployment!!"
+
+@app.route('/predict')
+def predict():
+    # Get values from browser
+    username = request.args['reviews_username']
+    d = item_final_rating.loc[username].sort_values(ascending=False)[0:20]
+    top_20_products = d.index.tolist()
+    print(top_20_products)
+    
+    sentiment_dict={}
+    for i in top_20_products:
+        reviews_list = prod_tfidf[i]
+        sentiment = [xgboost_model.predict(rev) for rev in reviews_list]
+        pos_sent = 100-((reduce(lambda x,y:x+y,sentiment)/len(sentiment))*100)
+        sentiment_dict[i] = pos_sent
+    print(sentiment_dict)
+    
+    
+    top_5 = [key for key,value in sorted(sentiment_dict.items(), key = lambda x:x[1], reverse=True)[:5]]
+    
+    final_list=[]
+    for prod in top_5:
+        final_list.append(prod)
+        
+    
+    return jsonify(final_list)
+    
+
+if __name__ == "__main__":
+    # Start Application
+    app.run()
